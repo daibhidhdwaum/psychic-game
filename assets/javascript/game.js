5ab9cbee3cc1364e1209ff4665a1e92ef9b79757
David Downie <daviddownie@gmail.com>
2018-10-11 23:22:16 -0400

    moved javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,56 @@
- //defines computer letter choices
- var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//defines computer letter choices
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
             
- //creates variable to hold number of wins, losses and guesses remaining.
- var wins = 0;
- var losses = 0;
- var guessesLeft = 9;
- var lettersGuessed = "";
+//creates variable to hold number of wins, losses and guesses remaining.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = "";
 
- var lettersGuessedText = document.getElementById("letters-guessed-text");
- var winsText = document.getElementById("wins-text");
- var lossesText = document.getElementById("losses-text");
- var guessesLeftText = document.getElementById("guesses-left-text");
+var lettersGuessedText = document.getElementById("letters-guessed-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
 
- var computerOutput;
 
- var computerOutput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
- 
- //resets the game 
- function newGame(){
-     guessesLeft = 9;
-     lettersGuessed = "";
-     //computer randomly chooses a letter from array
-     var computerOutput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-     console.log(computerOutput);
- }
+var computerOutput;
 
- //defines event that occurs when user makes choice
- document.onkeyup = function(event) {
+//runs new game when if/else statement parameters are fulfilled
+function newGame(){
+    guessesLeft = 9;
+    lettersGuessed = "";
+    computerOutput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    updateText();
+    console.log(computerOutput);
+}
+
+
+//defines event that occurs when user makes choice
+document.onkeyup = function(event) {
+   
+    //logs event
+   lettersGuessed = lettersGuessed + " " + event.key;
+
     
-     //functioning 
- lettersGuessed = lettersGuessed + " " + event.key;
-     
-     //console.log(lettersGuessed);
 
-     if (event.key === computerOutput){
-         wins++;
-         newGame();
-     } else {
-         guessesLeft--;
-         if (guessesLeft === -1){
-             losses++;
-             newGame();
-         }
-     }
-     updateText();
- };
- 
- function updateText (){
- //Display user input and wins, losses and guesses left;
- winsText.textContent = "Wins: " + wins;
- lossesText.textContent = "Losses: " + losses;
- guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
- lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
- }
+    if (event.key === computerOutput){
+        wins++;
+        newGame();
+    } else {
+        guessesLeft--;
+        if (guessesLeft === -1){
+            losses++;
+            newGame();
+        }
+    }
+    updateText();
+};
+
+function updateText (){
+//Display user input and wins, losses and guesses left;
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+}
+newGame();
